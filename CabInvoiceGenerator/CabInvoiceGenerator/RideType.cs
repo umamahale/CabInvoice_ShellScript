9f9e8d7ecac18e2e7deb990ab09ef52dd69c32ee
umamahale <umagasmos@gmail.com>
2021-10-22 08:45:46 -0400

    fisrt message

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/RideType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public enum RideType { NORMAL, PREMIUM }
+}
+
+
+
+
+
---
+++ CabInvoiceGenerator/CabInvoiceGeneratorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CabInvoiceGeneratorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CabInvoiceGeneratorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b6aff3b-8d62-4683-b293-2a0326458f53")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
